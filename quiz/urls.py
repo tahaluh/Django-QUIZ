5263fc01c51d6535b902232863f2e179ed4d4295
Thauã Magalhães <thauanlucascpl@gmail.com>
2024-05-12 21:16:57 -0300

    feat: generics rest generics added

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import QuizAPIView, QuestionAPIView, OptionAPIView
+from .views import QuizzesAPIView, QuizAPIView, QuestionsAPIView, QuestionAPIView, OptionsAPIView, OptionAPIView  # noqa E501
+
 
 urlpatterns = [
-    path('quiz/', QuizAPIView.as_view(), name='quiz'),
-    path('question/', QuestionAPIView.as_view(), name='question'),
-    path('option/', OptionAPIView.as_view(), name='option'),
+    path('quiz/', QuizzesAPIView.as_view(), name='quizes'),
+    path('quiz/<uuid:pk>/', QuizAPIView.as_view(), name='quiz'),
+
+    path('question/', QuestionsAPIView.as_view(), name='questions'),
+    path('question/<uuid:pk>/', QuestionAPIView.as_view(), name='question'),
+
+    path('option/', OptionsAPIView.as_view(), name='options'),
+    path('option/<uuid:pk>/', OptionAPIView.as_view(), name='option'),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,47 +1,33 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import generics
 from .models import Quiz, Question, Option
 from .serializers import QuizSerializer, QuestionSerializer, OptionSerializer
 
 
-class QuizAPIView(APIView):
-    def get(self, request):
-        quiz = Quiz.objects.all()
-        serializer = QuizSerializer(quiz, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = QuizSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class QuizzesAPIView(generics.ListCreateAPIView):
+    queryset = Quiz.objects.all()
+    serializer_class = QuizSerializer
 
 
-class QuestionAPIView(APIView):
-    def get(self, request):
-        question = Question.objects.all()
-        serializer = QuestionSerializer(question, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = QuestionSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class QuizAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Quiz.objects.all()
+    serializer_class = QuizSerializer
 
 
-class OptionAPIView(APIView):
-    def get(self, request):
-        option = Option.objects.all()
-        serializer = OptionSerializer(option, many=True)
-        return Response(serializer.data)
+class QuestionsAPIView(generics.ListCreateAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
 
-    def post(self, request):
-        serializer = OptionSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class QuestionAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+
+class OptionsAPIView(generics.ListCreateAPIView):
+    queryset = Option.objects.all()
+    serializer_class = OptionSerializer
+
+
+class OptionAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Option.objects.all()
+    serializer_class = OptionSerializer
