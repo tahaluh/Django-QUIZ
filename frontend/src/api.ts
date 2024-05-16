abfab3cede8a6a5b82b2dec26658e3bdc8a53d1e
Thauã Magalhães <thauanlucascpl@gmail.com>
2024-05-16 00:28:42 -0300

    feat: initial frontend added

---
+++ frontend/src/api.ts
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import { ACCESS_TOKEN } from './constants';
+import process from 'process';
+
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem(ACCESS_TOKEN);
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+
+);
+
+export default api;
